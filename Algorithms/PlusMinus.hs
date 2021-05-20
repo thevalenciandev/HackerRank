e8cdd5632b0a00c48d5904335738afee00d1a7f4
TVD <thevalenciandev@gmail.com>
2021-05-20 06:47:02 +0100

    Plus Minus solution

--- Algorithms/PlusMinus.hs
+++ Algorithms/PlusMinus.hs
@@ -2,4 +2,10 @@ main :: IO ()
 main = interact $ unlines . map show . solve . map read . words . last . lines
 
 solve :: [Int] -> [Float]
-solve xs = undefined
+solve xs = [ratpos,ratneg,ratzer]
+    where ratpos = ratio (>0)  xs
+          ratneg = ratio (<0)  xs
+          ratzer = ratio (==0) xs
+
+ratio :: (Int -> Bool) -> [Int] -> Float
+ratio p xs = fromIntegral (length(filter (p)  xs)) / fromIntegral (length xs)
