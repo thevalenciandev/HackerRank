d48a5da93b06c114ff955e1cd58499951350d05e
TVD <thevalenciandev@gmail.com>
2021-05-26 13:51:42 +0100

    Exiting earlier when calculating score.

--- Algorithms/Implementation/ClimbingTheLeaderboard.hs
+++ Algorithms/Implementation/ClimbingTheLeaderboard.hs
@@ -12,5 +12,4 @@ getList = map read . words <$> getLine
 solve :: [Int] -> [Int] -> [Int]
 solve board scores = [calcrank s | s <- scores]
     where flatboard      = map head $ group board
-          initialpos     = length flatboard + 1
-          calcrank score = foldr (\x acc -> if x <= score then acc-1 else acc) initialpos flatboard
+          calcrank score = (length $ takeWhile (>score) flatboard) + 1
