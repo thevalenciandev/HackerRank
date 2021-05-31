55ed6c41ad8ff05ec19ed1ddfd388ec08e8e1ef3
TVD <thevalenciandev@gmail.com>
2021-05-31 14:22:29 +0100

    Filter Positions in a List: pointless

--- FunctionalProgramming/Introduction/FilterPositionsInAList.hs
+++ FunctionalProgramming/Introduction/FilterPositionsInAList.hs
@@ -1,4 +1,4 @@
 main = interact $ unlines . map show . f . map read . words
 
 f :: [Int] -> [Int]
-f xs = map snd . filter (even . fst) . zip [1..] $ xs
+f = map snd . filter (even . fst) . zip [1..]
