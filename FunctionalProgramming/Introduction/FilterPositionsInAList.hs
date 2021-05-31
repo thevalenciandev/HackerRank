a2e3884e17c2098cd94c1942d7e9ab9fe9802043
TVD <thevalenciandev@gmail.com>
2021-05-31 14:22:00 +0100

    Filter Positions in a List: simplify

--- FunctionalProgramming/Introduction/FilterPositionsInAList.hs
+++ FunctionalProgramming/Introduction/FilterPositionsInAList.hs
@@ -1,4 +1,4 @@
 main = interact $ unlines . map show . f . map read . words
 
 f :: [Int] -> [Int]
-f xs = map snd . filter (\(i,_) -> even i) $ [(i,x) | (i,x) <- zip [1..length xs] xs]
+f xs = map snd . filter (even . fst) . zip [1..] $ xs
