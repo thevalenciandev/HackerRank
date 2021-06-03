d905e654c149312e777404b57a31d638ccbee374
TVD <thevalenciandev@gmail.com>
2021-06-03 06:30:45 +0100

    Camel Case refactor

--- Algorithms/Strings/CamelCase.hs
+++ Algorithms/Strings/CamelCase.hs
@@ -4,6 +4,8 @@ import Data.List
 main = interact $ show . solve . head . words
 
 solve :: String -> Int
-solve xs = 1 + length humps 
+solve xs = length (camelchunks xs)
+          
+camelchunks xs = fst firstsplit : humps
     where firstsplit = span isLower xs -- this will get separate the first word from the rest
           humps      = groupBy (\x y -> isUpper x && isLower y) $ snd firstsplit
